fb9f3d886f293dfef12c8be2f80aa58f05947f59
abhijitmondal121 <mondalavi9836@gmail.com>
2021-07-24 11:01:23 +0530

    update

--- parConstractor.cpp
+++ parConstractor.cpp
@@ -1,18 +1,18 @@
 #include<iostream>
 using namespace std;
-class con{
-    
+class con2{
+     
     int a,b;
-     public:
-    con(){
-        a=10;
-        b=9;
+    public:
+    con2(int num1,int num2){
+        a=num1;
+        b=num2;
     }
     void printData(){
         cout<<"sum : "<<a+b<<endl; 
     }
 };
 int main(){
-    con obj;
+    con2 obj(10,34);
     obj.printData();
 }
