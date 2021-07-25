2bf3486fd3fc7460b4f31dac87df19fd0597e609
abhijitmondal121 <mondalavi9836@gmail.com>
2021-07-25 21:59:50 +0530

    init

---
+++ array.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    
+    cout<<"enter the range : "<<endl;
+    cin>>n;
+    int a[n];
+    cout<<"enter the value : "<<endl;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    cout<<"the output : "<<endl;
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<endl;
+    }
+}
