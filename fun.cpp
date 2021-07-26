2db97d4c348a4381672e2a326bb84c67ce8ec39e
abhijitmondal121 <mondalavi9836@gmail.com>
2021-07-27 00:10:41 +0530

    init

---
+++ fun.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int dis(int x){
+    return x;
+}
+
+int main(){
+    int a;
+    cout<<"enter the number :"<<endl;
+    cin>>a;
+    cout<<"output : "<<dis(a)<<endl;
+}
