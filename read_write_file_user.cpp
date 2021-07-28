74a0d9f8365f0a379f6eff7fa36537e8d582f04b
abhijitmondal121 <mondalavi9836@gmail.com>
2021-07-28 09:19:47 +0530

    up

--- read_write_file_user.cpp
+++ read_write_file_user.cpp
@@ -5,21 +5,22 @@ int main(){
 
    
 // for writing in a file
-    string s;
-    ofstream out("test.txt");
-    cout<<"enter the string "<<endl;
-    cin>>s;
+    // string s;
+    // ofstream out("test.txt");
+    // cout<<"enter the string "<<endl;
+    // cin>>s;
     
-    out<<"My name is "+s;
-    out.close();
+    // out<<"My name is "+s;
+    // out.close();
 
 
 // for reading a file
-// string st1;
-//     ifstream in("test1.txt");
+string st1;
+    ifstream in("test1.txt");
 //     getline(in,st1);
-//     cout<<st1;
-
+    
+    in>>st1;
+    cout<<st1;
 
     return 0;
 }
