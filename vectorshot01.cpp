d264898d1fadcc58254488207338025c896f23e8
abhijitmondal121 <mondalavi9836@gmail.com>
2021-08-15 12:32:25 +0530

    in

--- vectorshot01.cpp
+++ vectorshot01.cpp
@@ -1,11 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-   
     cout<<"Enter the range : "<<endl;
     int n;
     cin>>n;
       vector<int>arr(n);
+      vector<int>arr1;
     cout<<"Enter the value : "<<endl;
     for (int i=0;i<n;i++){
         // int t;
@@ -13,12 +13,25 @@ int main(){
          cin>>arr[i];
         // arr.push_back(t);
     }
-    sort(arr.begin(),arr.end());
+    // sort(arr.begin(),arr.end());
         for (int i=0;i<n;i++){
+            if(arr[i]==0){
+                arr1.push_back(arr[i]);
+                // cout<<arr[i]<<" ";
+            }
+        }
+        // cout<<arr[i]<<" ";
+          for (int i=0;i<n;i++){
+            if(arr[i]==1){
+                arr1.push_back(arr[i]);
+                // cout<<arr[i]<<" ";
+            }   
+        }
 
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl;
+
+             for (int j=0;j<n;j++){
+                cout<<arr1[j]<<" ";
+            }
 
    
 
