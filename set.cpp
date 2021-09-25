5c24bc906d5b05cd1b612e0d06f0ae0cb8745227
ABHIJIT MONDAL <70851067+abhijitmondal121@users.noreply.github.com>
2021-09-25 21:47:11 +0530

    Add files via upload

---
+++ set.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<set>
+
+using namespace std;
+
+int main(){
+    
+    set<int>s;
+    s.insert(5);
+    s.insert(6);
+    s.insert(9);
+    s.insert(3);
+    s.insert(4);
+    
+    
+    // for(auto i:s){
+    //     cout<<i<<endl;
+    // }
+    
+    set<int>::iterator it=s.begin();
+    it++;
+    
+    s.erase(it);
+    
+    for(auto i:s){
+        cout<<i<<endl;
+    }
+    
+}
+
+
+
